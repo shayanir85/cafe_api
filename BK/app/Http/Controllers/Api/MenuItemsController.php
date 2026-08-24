7b6d24047764df4eeeb4b17d928aca6d3b5bfaef
iliyaPour <iliya.pourzanjani6@gmail.com>
2026-08-24 19:58:28 +0330

    Fixed Menu Not Working

--- BK/app/Http/Controllers/Api/MenuItemsController.php
+++ BK/app/Http/Controllers/Api/MenuItemsController.php
@@ -7,7 +7,6 @@ use App\Services\MenuService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Hekmatinasser\Verta\Verta;
 class MenuItemsController extends Controller
 {
     public function __construct(
@@ -33,8 +32,6 @@ class MenuItemsController extends Controller
     public function list(): JsonResponse
     {
         $menuItems = MenuItem::where('is_available', 1)->get();
-        
-        $menuItems->created_at = verta($menuItems->created_at);
 
         return response()->json([
             'success' => true,
